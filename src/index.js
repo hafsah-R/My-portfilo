1981c9ca68ca3f185a5dd010d4814b309fc8ab0b
hafsah-R <hafsahrizg@gmail.com>
2024-02-07 15:29:47 +0300

    hhhh

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import{Dropdown} from 'rsuite';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));h
+const root = ReactDOM.createRoot(document.getElementById('root'));hj
 root.render(
 <div>
 <header>
